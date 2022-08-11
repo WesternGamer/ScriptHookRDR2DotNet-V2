aaaa3f0cdb8f06e1ab4a1bbc19cb8e8538dbe3a7
Halen84 <55899402+Halen84@users.noreply.github.com>
2022-08-11 02:34:32 -0500

    Update ExampleScript.cs

--- examples/ExampleScript.cs
+++ examples/ExampleScript.cs
@@ -1,42 +1,45 @@
 ﻿using System;
 using System.Windows.Forms;
-using RDR2;
-using RDR2.UI;
-using RDR2.Native;
-using RDR2.Math;
+
+using RDR2;				// Generic RDR2 related stuff
+using RDR2.UI;			// UI related stuff
+using RDR2.Native;		// RDR2 native functions
+using RDR2.Math;		// Vectors, Quaternions, Matrixes
 
 namespace ExampleScript
 {
-    public class Main : Script
-    {
-        public Main()
-        {
-            KeyDown += OnKeyDown;
-            Tick += OnTick;
+	public class Main : Script
+	{
+		private bool bRagdoll = false;
 
-            Interval = 1;
-        }
+		public Main()
+		{
+			KeyDown += OnKeyDown; // Hook KeyDown event
+			Tick += OnTick; // Hook Tick event. This is called every frame
 
-        bool ragdoll = false;
+			Interval = 1; // Optional, default is already 1
+		}
 
-        private void OnTick(object sender, EventArgs e)
-        {
-            Ped playerPed = Game.Player.Character;
-            
-            if (ragdoll)
-            {
-                Function.Call(Hash.SET_PED_TO_RAGDOLL, playerPed, 5000, 5000, 0, false, false, false);
-            }
-        } 
+		private void OnTick(object sender, EventArgs e)
+		{
+			// Our current player ped
+			// Note: "Ped" is a class, not a type alias
+			Ped myPlayerPed = Game.Player.Character;
+			
+			if (bRagdoll)
+			{
+				PED.SET_PED_TO_RAGDOLL(myPlayerPed.Handle, 5000, 5000, 0, false, false, false);
+			}
+		} 
 
-        private void OnKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.C)
-            {
-                ragdoll = !ragdoll;
-				RDR2.UI.Screen.ShowSubtitle("Ragdoll");
-            }
-        }
-        
-    }
+		private void OnKeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.C)
+			{
+				bRagdoll = !bRagdoll;
+				RDR2.UI.Screen.PrintSubtitle("Ragdoll: " + bRagdoll.ToString());
+			}
+		}
+		
+	}
 }
