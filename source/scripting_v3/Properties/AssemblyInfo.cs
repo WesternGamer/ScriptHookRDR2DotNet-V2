219b0c0e77f62ac07def6f5f8e7e0e5f7500fbf7
Halen84 <55899402+Halen84@users.noreply.github.com>
2022-08-11 02:39:15 -0500

    Update binaries version

--- source/scripting_v3/Properties/AssemblyInfo.cs
+++ source/scripting_v3/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.5.5")]
-[assembly: AssemblyFileVersion("1.0.5.5")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
