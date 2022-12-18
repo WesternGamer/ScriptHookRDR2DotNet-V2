102bfca169e43d9cb07d081f41b649db5a5314fd
Halen84 <55899402+Halen84@users.noreply.github.com>
2022-12-18 15:36:05 -0600

    Fixes and changes from #1

--- source/scripting_v3/Properties/AssemblyInfo.cs
+++ source/scripting_v3/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScriptHookRDRNetAPI")]
-[assembly: AssemblyDescription("The .NET scripting API for RDR2")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScriptHookRDRNetAPI")]
+//[assembly: AssemblyTitle("ScriptHookRDRNetAPI")]
+//[assembly: AssemblyDescription("The .NET scripting API for RDR2")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("ScriptHookRDRNetAPI")]
 [assembly: AssemblyCopyright("Copyright © 2020 Salty | Copyright © 2015 crosire")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+//[assembly: AssemblyVersion("2.0.0.0")]
+//[assembly: AssemblyFileVersion("2.0.0.0")]
