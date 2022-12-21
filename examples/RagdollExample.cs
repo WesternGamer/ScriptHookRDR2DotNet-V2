a12b2b51ba287b134262f63d79fc3ae35ad9b792
Halen84 <55899402+Halen84@users.noreply.github.com>
2022-12-21 17:34:06 -0600

    Add some very basic examples

--- examples/RagdollExample.cs
+++ examples/RagdollExample.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Windows.Forms;
 
-using RDR2;				// Generic RDR2 related stuff
-using RDR2.UI;			// UI related stuff
-using RDR2.Native;		// RDR2 native functions (commands)
-using RDR2.Math;		// Vectors, Quaternions, Matrixes
+using RDR2;             // Generic RDR2 related stuff
+using RDR2.UI;          // UI related stuff
+using RDR2.Native;      // RDR2 native functions (commands)
+using RDR2.Math;        // Vectors, Quaternions, Matrixes
 
-namespace ExampleScript
+namespace RagdollExample
 {
 	public class Main : Script
 	{
@@ -16,7 +16,6 @@ namespace ExampleScript
 		{
 			KeyDown += OnKeyDown; // Hook KeyDown event
 			Tick += OnTick; // Hook Tick event. This is called every frame
-
 			Interval = 1; // Optional, default is 0
 		}
 
@@ -32,7 +31,7 @@ namespace ExampleScript
 			{
 				PED.SET_PED_TO_RAGDOLL(myPlayerPed.Handle, 5000, 5000, 0, false, false, string.Empty);
 			}
-		} 
+		}
 
 		private void OnKeyDown(object sender, KeyEventArgs e)
 		{
@@ -42,6 +41,5 @@ namespace ExampleScript
 				RDR2.UI.Screen.PrintSubtitle("Ragdoll: " + bRagdoll.ToString());
 			}
 		}
-		
 	}
 }
